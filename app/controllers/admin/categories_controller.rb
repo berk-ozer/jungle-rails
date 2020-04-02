e1b39f07eae0fdad39b6c0371699e4344bab04c8
Berk Ozer <galipberkozer@gmail.com>
2020-04-02 11:23:17 -0400

    Build new and create actions for admin/categories

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -2,4 +2,18 @@ class Admin::CategoriesController < ApplicationController
   def index
     @categories = Category.order(:name).all
   end
+
+  def new
+    @category = Category.new
+  end
+
+  def create 
+    @category = Category.new(params.require(:category).permit(:name))
+
+    if @category.save
+      redirect_to [:admin, :categories], notice: 'Category created!'
+    else
+      render :new
+    end
+  end
 end
