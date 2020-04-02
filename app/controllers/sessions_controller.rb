e6573c02ea721e9ba43b9862a0d66a25551d6691
Berk Ozer <galipberkozer@gmail.com>
2020-04-02 14:45:00 -0400

    Make user a regular var instead of instance var

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,13 +3,13 @@ class SessionsController < ApplicationController
   end
 
   def create
-    @user = User.find_by_email(params[:email])
+    user = User.find_by_email(params[:email])
 
     # If user exists and password entered is correct
-    if @user && @user.authenticate(params[:password])
+    if user && user.authenticate(params[:password])
 
       # Save user id as cookie so they stay logged in
-      session[:user_id] = @user.id
+      session[:user_id] = user.id
       redirect_to :root
 
     else 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,10 +3,10 @@ class UsersController < ApplicationController
   end
 
   def create  
-    @user = User.new(user_params)
+    user = User.new(user_params)
 
-    if @user.save
-      session[:user_id] = @user.id
+    if user.save
+      session[:user_id] = user.id
       redirect_to :root, notice: 'User created!'
     else
       render :new
