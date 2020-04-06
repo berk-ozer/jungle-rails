b839dcab9d4d88664ee3d7d1e1b724f2c7832202
Berk Ozer <galipberkozer@gmail.com>
2020-04-06 16:54:39 -0400

    Add test to see product is valid when created with valid attributes in product_spec

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
-RSpec.describe ProductSpec, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe Product, type: :model do
+  
+  let(:category) {
+    Category.new(name: "testCategory")
+  }
+  subject {
+    category.products.new(
+      name: "testProduct",
+      description: "this is a test product",
+      image: "testImage",
+      price_cents: 995,
+      quantity: 15,
+    )
+  }
+
+  describe "Validations" do
+    it "is valid with valid attributes" do
+      expect(subject).to be_valid
+      expect(subject.errors.full_messages).to be_empty
+    end
+  end
 end
