28c1e79f4a535ffac939974e0c8ff5a46547128a
Berk Ozer <galipberkozer@gmail.com>
2020-04-06 17:14:55 -0400

    Change testing method in product_spec: See if error array includes text, instead of just checking first element in error array

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -24,7 +24,7 @@ RSpec.describe Product, type: :model do
     it "is not valid without a name" do
       subject.name = nil
       expect(subject).to_not be_valid
-      expect(subject.errors.full_messages.first).to eq "Name can't be blank"
+      expect(subject.errors.full_messages).to include "Name can't be blank"
     end
 
     it "is not valid without a price" do
