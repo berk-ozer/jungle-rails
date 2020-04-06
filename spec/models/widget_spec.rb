033848a9a3b8600d0c66d83f6bd4d225f37f9c2e
Berk Ozer <galipberkozer@gmail.com>
2020-04-06 16:09:06 -0400

    Add dummy tests for the test widget model

--- spec/models/widget_spec.rb
+++ spec/models/widget_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
-RSpec.describe Widget, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe '#id' do
+  it 'should not exist for new records' do
+    @widget = Widget.new
+    expect(@widget.id).to be_nil
+  end
+
+  it 'should be auto-assigned by AR for saved records' do
+    @widget = Widget.new
+    # we use bang here b/c we want our spec to fail if save fails (due to validations)
+    # we are not testing for successful save so we have to assume it will be successful
+    @widget.save!
+
+    expect(@widget.id).to be_present
+  end
 end
