26a0c9b59d4bf56a89502e0c2aa1b8c98aa7cded
Berk Ozer <galipberkozer@gmail.com>
2020-04-06 17:05:57 -0400

    Add test in product_spec: Product is not valid without a name

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -20,5 +20,11 @@ RSpec.describe Product, type: :model do
       expect(subject).to be_valid
       expect(subject.errors.full_messages).to be_empty
     end
+
+    it "is not valid without a name" do
+      subject.name = nil
+      expect(subject).to_not be_valid
+      expect(subject.errors.full_messages.first).to eq "Name can't be blank"
+    end
   end
 end
