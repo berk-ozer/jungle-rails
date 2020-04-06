ef4c6af889e4115d6b1a4c2127c577717e519bb6
Berk Ozer <galipberkozer@gmail.com>
2020-04-06 17:13:24 -0400

    Add test to product_spec: Product not valid without a price

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -26,5 +26,11 @@ RSpec.describe Product, type: :model do
       expect(subject).to_not be_valid
       expect(subject.errors.full_messages.first).to eq "Name can't be blank"
     end
+
+    it "is not valid without a price" do
+      subject.price_cents = nil
+      expect(subject).to_not be_valid
+      expect(subject.errors.full_messages).to include "Price can't be blank"
+    end
   end
 end
