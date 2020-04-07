36a03aeebc86002b1dc690ce52e956b9d296ca57
Berk Ozer <galipberkozer@gmail.com>
2020-04-07 15:16:21 -0400

    Remove unused save_screenshot in home_page_spec

--- spec/features/home_page_spec.rb
+++ spec/features/home_page_spec.rb
@@ -22,8 +22,6 @@ RSpec.feature "Visitor navigates to home page", type: :feature, js: true do
     visit root_path
 
     # DEBUG / VERIFY
-    # save_screenshot
-
     expect(page).to have_css "article.product", count: 10
   end
 
