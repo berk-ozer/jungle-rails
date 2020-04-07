dcf14d254c49d0e893fc474a54ce832740e77bb2
Berk Ozer <galipberkozer@gmail.com>
2020-04-07 14:24:48 -0400

    Comment out "save_screenshot" in home_page_spec first test

--- spec/features/home_page_spec.rb
+++ spec/features/home_page_spec.rb
@@ -22,7 +22,7 @@ RSpec.feature "Visitor navigates to home page", type: :feature, js: true do
     visit root_path
 
     # DEBUG / VERIFY
-    save_screenshot
+    # save_screenshot
 
     expect(page).to have_css "article.product", count: 10
   end
