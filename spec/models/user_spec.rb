86397b9795ff6d6a4c31c2ff05e85ee1d47810c8
Berk Ozer <galipberkozer@gmail.com>
2020-04-06 23:05:20 -0400

    Add test to user_spec: User not valid without password confirmation

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -41,6 +41,12 @@ RSpec.describe User, type: :model do
       expect(subject.errors.full_messages).to include ("Password can't be blank")
     end
 
+    it "is not valid without pasword confirmation" do
+      subject.password_confirmation = nil
+      expect(subject).to_not be_valid
+      expect(subject.errors.full_messages).to include ("Password confirmation can't be blank")
+    end
+
     it "is not valid when password and password_confirmation don't match" do
       subject.password_confirmation = "notsecret"
       expect(subject).to_not be_valid
