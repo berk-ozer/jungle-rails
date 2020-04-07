00c6a0480777e1b801538b35eb4e818bda30c244
Berk Ozer <galipberkozer@gmail.com>
2020-04-06 23:01:33 -0400

    Refactor tests in user_spec to use "subject" instead of creating a new User in each test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,60 +1,44 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  subject {
+    described_class.new(
+      first_name: "Berk", 
+      last_name: "Ozer", 
+      email: "berk@gmail.com", 
+      password: "secret", 
+      password_confirmation: "secret"
+    )
+  }
 
   describe "Validations" do
     it "is valid with valid attributes" do
-      user = User.create(
-        first_name: "Berk", 
-        last_name: "Ozer", 
-        email: "galipberkozer@gmail.com", 
-        password: "secret", 
-        password_confirmation: "secret"
-      )
-      expect(user).to be_valid
-      expect(user.errors.full_messages).to be_empty
+      expect(subject).to be_valid
+      expect(subject.errors.full_messages).to be_empty
     end
 
     it "is not valid without first name" do
-      user = User.create( 
-        last_name: "Ozer", 
-        email: "galipberkozer@gmail.com", 
-        password: "secret", 
-        password_confirmation: "secret"
-      )
-      expect(user).to_not be_valid
-      expect(user.errors.full_messages).to include ("First name can't be blank")
+      subject.first_name = nil
+      expect(subject).to_not be_valid
+      expect(subject.errors.full_messages).to include ("First name can't be blank")
     end
 
     it "is not valid when password and password_confirmation don't match" do
-      user = User.create(
-        first_name: "Test", 
-        last_name: "Person", 
-        email: "test1@gmail.com", 
-        password: "something", 
-        password_confirmation: "else"
-      )
-      expect(user).to_not be_valid
-      expect(user.errors.full_messages).to include ("Password confirmation doesn't match Password")
+      subject.password_confirmation = "notsecret"
+      expect(subject).to_not be_valid
+      expect(subject.errors.full_messages).to include ("Password confirmation doesn't match Password")
     end
 
     it "is not valid when email isn't unique (case insensitive)" do
-      user = User.create(
+      same_as_subject = User.create(
         first_name: "Test", 
         last_name: "Person", 
-        email: "same@gmail.com", 
+        email: "BERK@gmail.com", 
         password: "secret", 
         password_confirmation: "secret"
       )
-      same_user = User.create(
-        first_name: "Test", 
-        last_name: "Person", 
-        email: "SAME@gmail.com", 
-        password: "secret", 
-        password_confirmation: "secret"
-      )
-      expect(same_user).to_not be_valid
-      expect(same_user.errors.full_messages).to include ("Email has already been taken")
+      expect(subject).to_not be_valid
+      expect(subject.errors.full_messages).to include ("Email has already been taken")
     end
   end
 end
