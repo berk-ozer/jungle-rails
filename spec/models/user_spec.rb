8339fcd69a824b0d722fd9e4cb42285f470ff949
Berk Ozer <galipberkozer@gmail.com>
2020-04-06 22:03:49 -0400

    User.create instead of User.new in user_spec tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,13 +4,13 @@ RSpec.describe User, type: :model do
 
   describe "Validations" do
     it "is valid with valid attributes" do
-      user = User.new(first_name: "Berk", last_name: "Ozer", email: "galipberkozer@gmail.com", password: "secret", password_confirmation: "secret")
+      user = User.create(first_name: "Berk", last_name: "Ozer", email: "galipberkozer@gmail.com", password: "secret", password_confirmation: "secret")
       expect(user).to be_valid
       expect(user.errors.full_messages).to be_empty
     end
 
     it "is not valid when password and password_confirmation don't match" do
-      user = User.new(first_name: "Test", last_name: "Person", email: "test1@gmail.com", password: "something", password_confirmation: "else")
+      user = User.create(first_name: "Test", last_name: "Person", email: "test1@gmail.com", password: "something", password_confirmation: "else")
       expect(user).to_not be_valid
       expect(user.errors.full_messages).to include ("Password confirmation doesn't match Password")
     end
