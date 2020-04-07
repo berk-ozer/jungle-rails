b651b1fae72ad1e668a5b4258226e03fdcb9dddb
Berk Ozer <galipberkozer@gmail.com>
2020-04-06 23:14:14 -0400

    Add validation to User model to have password be at least 5 characters long

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,6 @@ class User < ActiveRecord::Base
   validates :email, presence: true, uniqueness: { case_sensitive: false }
   validates :first_name, presence: true
   validates :last_name, presence: true
-  validates :password, presence: true
+  validates :password, presence: true, length: { minimum: 5 }
   validates :password_confirmation, presence: true
 end
