0478dae1e42877d15cfa1a1ed30294e74908921a
Berk Ozer <galipberkozer@gmail.com>
2020-04-06 22:41:44 -0400

    Add validation of presence for first/last name, password/confirmation in User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,8 @@ class User < ActiveRecord::Base
   has_secure_password
 
   validates :email, presence: true, uniqueness: { case_sensitive: false }
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :password, presence: true
+  validates :password_confirmation, presence: true
 end
