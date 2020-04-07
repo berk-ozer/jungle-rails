d68e42591517ae03cda096cccee41c6f07788208
Berk Ozer <galipberkozer@gmail.com>
2020-04-07 11:29:32 -0400

    Build authenticate_with_credentials class method in User model  which takes over  auth logic from sessions_controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,14 @@ class User < ActiveRecord::Base
   validates :last_name, presence: true
   validates :password, presence: true, length: { minimum: 5 }
   validates :password_confirmation, presence: true
+
+  def self.authenticate_with_credentials(email, password)
+    user = User.find_by_email(email)
+    # If user exists and password is correct
+    if user & user.authenticate(password)
+      user
+    else
+      nil
+    end
+  end
 end
