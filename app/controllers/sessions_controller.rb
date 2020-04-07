4c1b44d88f06b137dfb8bc56d6df895d8d2341c4
Berk Ozer <galipberkozer@gmail.com>
2020-04-07 11:30:01 -0400

    Use the new User.authenticate_with_credentials method in sessions_controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,19 +3,14 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by_email(params[:email])
-
-    # If user exists and password entered is correct
-    if user && user.authenticate(params[:password])
-
+    # If user is successfully authenticated
+    if user = User.authenticate_with_credentials(params[:email], params[:password])
       # Save user id as cookie so they stay logged in
       session[:user_id] = user.id
       redirect_to :root
-
     else 
       render :new
     end
-    
   end
 
   def destroy
